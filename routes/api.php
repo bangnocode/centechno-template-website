f31e8efecb0d8efde2048b1491d5be8ea1f7d594
inocode <fachrezyofficial@gmail.com>
2025-09-16 12:58:30 +0700

    ganti endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/product', function () {
+Route::get('/test', function () {
     return response()->json([
         'status' => 'success',
         'data' => 'test response api hosting',
